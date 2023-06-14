78cee2ed2372393e096b257a6cfab0b7deff3fd8
n-itachi <125132601+Nguimbous-Itachi@users.noreply.github.com>
2023-06-14 21:46:36 +0100

    simple commit

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -42,6 +42,7 @@ resource "azurerm_mssql_server" "udacity" {
   tags = {
     environment = "udacity"
   }
+  
 }
 
 resource "azurerm_app_service_plan" "udacity" {
@@ -53,19 +54,22 @@ resource "azurerm_app_service_plan" "udacity" {
     tier = "Basic"
     size = "B1"
   }
+
 }
 
 resource "azurerm_windows_web_app" "udacity" {
   name                = "udacity-nikita-azure-dotnet-app"
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
-  app_service_plan_id = data.azurerm_app_service_plan.udacity.id
+  service_plan_id     = azurerm_app_service_plan.udacity.id
   
   site_config {
-    dotnet_framework_version = "v5.0"
+    always_on = true
+    dotnet_version = "5.0"
   }
   
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
+
 }
